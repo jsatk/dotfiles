fa643aea00362b6ca282439728eb32da2e1e5d8b
Jesse Atkinson <jatkinson@tunein.com>
2013-11-19 19:34:29 -0800

    Added Steve Losh's prompt to my config.fish.

--- config.fish
+++ config.fish
@@ -172,3 +172,39 @@ end
 function singletest -d 'Run single rake spec'
   rake spec SPEC=$argv
 end
+
+# ======
+# Prompt
+# ======
+
+function fish_prompt --description 'Write out the prompt'
+  set -l last_status $status
+
+  echo
+
+  set_color magenta
+  printf '%s' (whoami)
+  set_color normal
+  printf ' at '
+
+  set_color yellow
+  printf '%s' (hostname|cut -d . -f 1)
+  set_color normal
+  printf ' in '
+
+  set_color $fish_color_cwd
+  printf '%s' (prompt_pwd)
+  set_color normal
+__terlar_git_prompt
+  echo
+
+  if test $last_status -eq 0
+    set_color normal
+    printf '><((°> '
+  else
+    set_color $fish_color_error
+    printf '[%d] ><((ˣ> ' $last_status
+  end
+
+  set_color normal
+end
