443b148546c5655b2a473250ef448da3b6cb5b13
Jesse Atkinson <jesse.atkinson@me.com>
2016-09-01 19:31:21 -0700

    Remove unnecessary dotfiles and add make syntax support to vimrc

--- functions.symlink/updatevim.fish
+++
@@ -1,4 +0,0 @@
-function updatevim --description 'Required to use Vundle. See: https://github.com/gmarik/Vundle.vim/wiki#faq4'
-	set -lx SHELL (which sh)
-    vim +BundleInstall! +BundleClean +qall
-end
