b86b7f062094a7ac66241faa21d6842f7c6d0640
Jesse Atkinson <jesse@jsatk.us>
2018-11-08 13:29:42 -0800

    Add comment to now-playing script

--- scripts/now-playing.js
+++ scripts/now-playing.js
@@ -11,13 +11,17 @@
   const spotify = Application('Spotify')
   const itunesIsPlaying = itunes.running() && itunes.playerState() === 'playing'
   const spotifyIsPlaying = spotify.running() && spotify.playerState() === 'playing'
-  // Trim string at the 50th character.  Change this if you wish for a
-  // different max length or remove the `.substr` calls entirely if you wish
-  // for no trimming.
-  const trim = 50
+  // Trim string at the 64th character.  Why 64?  Because `pinkMusicNote` is 34
+  // characters long and 30 + 34 = 64.  30 characters seems like a reasonable
+  // max for song title.
+  //
+  // Change this if you wish for a different max length or
+  // remove the `.substr` calls entirely if you wish for no trimming.
+  const trim = 64
 
   if (itunesIsPlaying) {
     const track = itunes.currentTrack
+    // Not used currently but leaving here for easy future use if I change my mind.
     const artist = track.artist()
     const title = track.name()
 
@@ -26,6 +30,7 @@
 
   if (spotifyIsPlaying) {
     const track = spotify.currentTrack
+    // Not used currently but leaving here for easy future use if I change my mind.
     const artist = track.artist()
     const title = track.name()
 
