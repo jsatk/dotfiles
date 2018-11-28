d786041dc1059f789f857cf2d7a70c6875f7be6d
Jesse Atkinson <jesse@jsatk.us>
2018-11-28 15:49:04 -0800

    Update fish_prompt
    Use good ol' pre-installed prompt.  Stop being fussy.

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,67 +1,87 @@
-function fish_prompt --description "Sets the Fish Shell prompt"
-	 ~/go/bin/powerline-go \
-#	Usage of powerline-go:
-#		-colorize-hostname
-#			Colorize the hostname based on a hash of itself
-#		-cwd-max-depth int
-#			Maximum number of directories to show in path
-#				(default 5)
-#		-cwd-max-dir-size int
-#			Maximum number of letters displayed for each directory in the path
-#				(default -1)
-#		-cwd-mode string
-#			How to display the current directory
-#				(valid choices: fancy, plain, dironly)
-#				(default "fancy")
-#		-duration string
-#			The number of wall-clock seconds elapsed for the previous command.  May be an integer (whole seconds) or floating point (seconds and fractions of a second).
-#		-east-asian-width
-#			Use East Asian Ambiguous Widths
-#		-error int
-#			Exit code of previously executed command
-		-error $status \
-#		-ignore-repos string
-#			A list of git repos to ignore. Separate with ','.
-#				Repos are identified by their root directory.
-#		-max-width int
-#			Maximum width of the shell that the prompt may use, in percent. Setting this to 0 disables the shrinking subsystem.
-#		-mode string
-#			The characters used to make separators between segments.
-#				(valid choices: patched, compatible, flat)
-#				(default "patched")
-		-mode "flat" \
-#		-modules string
-#			The list of modules to load, separated by ','
-#				(valid choices: aws, cwd, docker, dotenv, duration, exit, git, gitlite, hg, host, jobs, load, nix-shell, perlbrew, perms, root, shell-var, ssh, termtitle, time, user, venv, node)
-#				(default "venv,user,host,ssh,cwd,perms,git,hg,jobs,exit,root")
-		-modules "docker,ssh,cwd,gitlite,hg,root" \
-#		-newline
-#			Show the prompt on a new line
-#		-numeric-exit-codes
-#			Shows numeric exit codes for errors.
-#		-path-aliases string
-#			One or more aliases from a path to a short name. Separate with ','.
-#				An alias maps a path like foo/bar/baz to a short name like FBB.
-#				Specify these as key/value pairs like foo/bar/baz=FBB.
-#				Use '~' for your home dir. You may need to escape this character to avoid shell substitution.
-#		-priority string
-#			Segments sorted by priority, if not enough space exists, the least priorized segments are removed first. Separate with ','
-#				(valid choices: aws, cwd, cwd-path, docker, duration, exit, git-branch, git-status, hg, host, jobs, load, nix-shell, perlbrew, perms, root, ssh, time, user, venv, node)
-#				(default "root,cwd,user,host,ssh,perms,git-branch,git-status,hg,jobs,exit,cwd-path")
-#		-shell string
-#			Set this to your shell type
-#				(valid choices: bare, bash, zsh)
-#				(default "bash")
-		-shell "bare"
-#		-shell-var string
-#			A shell variable to add to the segments.
-#		-shorten-gke-names
-#			Shortens names for GKE Kube clusters.
-#		-theme string
-#			Set this to the theme you want to use
-#				(valid choices: default, low-contrast)
-#				(default "default")
-#		-truncate-segment-width int
-#			Minimum width of a segment, segments longer than this will be shortened if space is limited. Setting this to 0 disables it.
-#				(default 16)
+function fish_prompt
+	set -l __last_command_exit_status $status
+
+    if not set -q -g __fish_robbyrussell_functions_defined
+        set -g __fish_robbyrussell_functions_defined
+        function _git_branch_name
+            set -l branch (git symbolic-ref --quiet HEAD ^/dev/null)
+            if set -q branch[1]
+                echo (string replace -r '^refs/heads/' '' $branch)
+            else
+                echo (git rev-parse --short HEAD ^/dev/null)
+            end
+        end
+
+        function _is_git_dirty
+            echo (git status -s --ignore-submodules=dirty ^/dev/null)
+        end
+
+        function _is_git_repo
+            type -q git
+            or return 1
+            git status -s >/dev/null ^/dev/null
+        end
+
+        function _hg_branch_name
+            echo (hg branch ^/dev/null)
+        end
+
+        function _is_hg_dirty
+            echo (hg status -mard ^/dev/null)
+        end
+
+        function _is_hg_repo
+            type -q hg
+            or return 1
+            hg summary >/dev/null ^/dev/null
+        end
+
+        function _repo_branch_name
+            eval "_$argv[1]_branch_name"
+        end
+
+        function _is_repo_dirty
+            eval "_is_$argv[1]_dirty"
+        end
+
+        function _repo_type
+            if _is_hg_repo
+                echo 'hg'
+            else if _is_git_repo
+                echo 'git'
+            end
+        end
+    end
+
+    set -l cyan (set_color -o cyan)
+    set -l yellow (set_color -o yellow)
+    set -l red (set_color -o red)
+    set -l green (set_color -o green)
+    set -l blue (set_color -o blue)
+    set -l normal (set_color normal)
+
+    set -l arrow_color "$green"
+    if test $__last_command_exit_status != 0
+        set arrow_color "$red"
+    end
+
+    set -l arrow "$arrow_color➜ "
+    if test "$USER" = 'root'
+        set arrow "$arrow_color# "
+    end
+
+    set -l cwd $cyan(basename (prompt_pwd))
+
+    set -l repo_type (_repo_type)
+    if [ $repo_type ]
+        set -l repo_branch $red(_repo_branch_name $repo_type)
+        set repo_info "$blue $repo_type:($repo_branch$blue)"
+
+        if [ (_is_repo_dirty $repo_type) ]
+            set -l dirty "$yellow ✗"
+            set repo_info "$repo_info$dirty"
+        end
+    end
+
+    echo -n -s $arrow ' '$cwd $repo_info $normal ' '
 end
