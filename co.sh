1157ed59c36391e02b4295a288c8f6139d7d8ef7
Jesse Atkinson <jesse@jsatk.us>
2018-12-06 09:29:04 -0800

    Remove co.sh method that snuck in

--- co.sh
+++
@@ -1,9 +0,0 @@
-!f() {
-  if [ -n \"$1\" -a -z \"$2\" ]; then 
-    git checkout "`revisionid-to-diffid.sh --tag \"$@\"`";
-  else
-    git checkout \"$@\";
-  fi && git submodule update --init --recursive; 
-}
-
-f
