3145ff827e6c884c83ade9dcd2209b5b67a05d56
Jesse Atkinson <jesse.atkinson@creditkarma.com>
2019-11-08 11:05:29 -0800

    Add thrift support to vim

--- tag-etc/config/fish/functions/ll.fish
+++ tag-etc/config/fish/functions/ll.fish
@@ -1,3 +1,4 @@
+# Defined in /var/folders/d6/55nbd6lj26952tnnxgbmydqdmrmpnd/T//fish.oSFT8S/ll.fish @ line 2
 function ll --description 'List all files colorized in long format, including dot files.'
-	ls -al $argv
+	exa -al $argv
 end
