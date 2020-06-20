7a2c9a891a879e7d4b43abe74f10957b9ab25a65
Jesse Atkinson <jesse.atkinson@creditkarma.com>
2020-06-19 19:34:20 -0700

    Added newsletters to mutt & fixed ep

--- tag-etc/config/fish/functions/ep.fish
+++ tag-etc/config/fish/functions/ep.fish
@@ -1,6 +1,10 @@
-function ep --description "Edit .plan"
-    git -C ~/.plan/ pull
-    vim +Goyo ~/.plan
-    git -C ~/.plan/ commit -am "Update"
-    git -C ~/.plan/ push
+function ep --description 'Edit .plan'
+    git -C ~/Developer/jsatk.us pull
+    set ymd (date "+%m-%d-%Y")
+    set cmd1 "normal! gg}}}}}O    * [$ymd](#$ymd)"
+    set cmd2 'exe "normal! Go\<esc>o\<esc>"'
+    set cmd3 "normal! i## $ymd"
+    vim +Goyo -c "$cmd1" -c "$cmd2" -c "$cmd3" ~/Developer/jsatk.us/content/dotplan.md
+    git -C ~/Developer/jsatk.us commit -am "Update .plan"
+    git -C ~/Developer/jsatk.us push
 end
