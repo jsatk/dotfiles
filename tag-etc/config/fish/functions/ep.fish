33086dbaee268b674e71da8e73f5ce8bd40dc320
Jesse Atkinson <jesse@jsatk.us>
2020-09-05 11:24:37 -0700

    vim -> nvim

--- tag-etc/config/fish/functions/ep.fish
+++ tag-etc/config/fish/functions/ep.fish
@@ -4,7 +4,7 @@ function ep --description "Edit .plan"
     set cmd1 "normal! gg}}}}}O    * [$ymd](#$ymd)"
     set cmd2 'exe "normal! Go\<esc>o\<esc>"'
     set cmd3 "normal! i## $ymd"
-    vim +Goyo -c "$cmd1" -c "$cmd2" -c "$cmd3" ~/Developer/jsatk.us/content/dotplan.md
+    nvim +Goyo -c "$cmd1" -c "$cmd2" -c "$cmd3" ~/Developer/jsatk.us/content/dotplan.md
     git -C ~/Developer/jsatk.us commit -am "Update .plan"
     git -C ~/Developer/jsatk.us push
 end
