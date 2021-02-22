1ec4ae9762601394027255275e73a83af50b5dc4
Jesse Atkinson <jesse.atkinson@creditkarma.com>
2021-02-21 18:17:38 -0800

    Update edit vim & nvim commands

---
+++ tag-etc/config/fish/functions/env.fish
@@ -0,0 +1,3 @@
+function env --description Edit\ neovim\'s\ init.lua
+	eval nvim ~/.config/nvim/init.lua # Explicitly open it in nvim, not what $EDITOR is set to.
+end
--- tag-etc/config/fish/functions/ev.fish
+++ tag-etc/config/fish/functions/ev.fish
@@ -1,3 +1,4 @@
-function ev --description "Edit .vimrc"
-	eval $EDITOR ~/.vimrc
+# Defined in /var/folders/ld/jpkqdkzn5f71slwtlmwzmf100000gp/T//fish.KtXPKt/ev.fish @ line 2
+function ev --description 'Edit .vimrc'
+	eval vim ~/.vimrc # Explicitly open it in vim, not what $EDITOR is set to.
 end
