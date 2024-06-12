89b5d68cd23a025d9b55409315be4a2c0dde6f2b
Jesse Atkinson <jesse.atkinson@creditkarma.com>
2024-06-11 20:21:40 -0700

    Update Java JDK to 11 because Metals doesn't support 8 anmyore

--- tag-vim/config/nvim/lua/plugins/metals.lua
+++ tag-vim/config/nvim/lua/plugins/metals.lua
@@ -1,19 +1,25 @@
+local ft = { "sbt", "scala", "java" }
 return {
   {
     "scalameta/nvim-metals",
-    ft = { "sbt", "scala" },
-    dependencies = { "nvim-lua/plenary.nvim" },
+    ft = ft,
+    dependencies = { "nvim-lua/plenary.nvim", "mfussenegger/nvim-dap" },
     config = function()
       local metals_config = require("metals").bare_config()
 
       metals_config.settings = {
         showImplicitArguments = true,
         excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
+        autoImportBuild = "all",
+        defaultBspToBuildTool = true,
       }
 
       metals_config.init_options.statusBarProvider = "on"
+
+      -- Example if you are using cmp how to make sure the correct capabilities for snippets are set
       metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
 
+      -- Debug settings if you're using nvim-dap
       local dap = require("dap")
 
       dap.configurations.scala = {
@@ -43,10 +49,7 @@ return {
       -- Autocmd that will actually be in charging of starting the whole thing
       local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
       vim.api.nvim_create_autocmd("FileType", {
-        -- NOTE: You may or may not want java included here. You will need it if you
-        -- want basic Java support but it may also conflict if you are using
-        -- something like nvim-jdtls which also works on a java filetype autocmd.
-        pattern = { "scala", "sbt", "java" },
+        pattern = ft,
         callback = function()
           require("metals").initialize_or_attach(metals_config)
         end,
