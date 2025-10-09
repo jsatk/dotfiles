63a761f8b3999f963f9c0b04abfbc86c6a42e6d5
Jesse Atkinson <jesse@jsatk.us>
2025-10-08 18:24:05 -0700

    Initial commit of entire redux of dotfiles

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,13 @@
+# Environment Variables {{{1
+set --export EDITOR nvim
+
+# Files I need to source {{{1
+
+[ -f /opt/homebrew/share/autojump/autojump.fish ]; and source /opt/homebrew/share/autojump/autojump.fish
+
+# Default fish settings for interactive sessions {{{1
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# vim: foldmethod=marker foldlevel=0
--- tag-vim/config/nvim/lua/plugins/nvim-metals.lua
+++
@@ -1,8 +0,0 @@
-return {
-  {
-    "scalameta/nvim-metals",
-    opts = {
-      javaHome = "/Library/Java/JavaVirtualMachines/amazon-corretto-8.jdk/Contents/Home",
-    },
-  },
-}
