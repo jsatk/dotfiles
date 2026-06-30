b87cc88437baf06c5a9380d1d0e17d4823ed21ef
Jesse Atkinson <jesse.atkinson@gusto.com>
2026-06-30 09:47:51 -0700

    Update `ev` to modify neovim config, not .vimrc

--- fish/.config/fish/functions/ev.fish
+++ fish/.config/fish/functions/ev.fish
@@ -1,4 +1,4 @@
 # Defined in /var/folders/ld/jpkqdkzn5f71slwtlmwzmf100000gp/T//fish.KtXPKt/ev.fish @ line 2
-function ev --description 'Edit .vimrc'
-	eval $EDITOR ~/.vimrc
+function ev --description 'Edit neovim config'
+    eval $EDITOR ~/.config/nvim/
 end
